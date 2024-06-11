79716520c0551b3997ec86d0affe57dbfad355c9
seonmo <jeongahkim8174@gmail.com>
2024-06-11 15:36:04 -0700

    added the down arrow  thing

--- src/Contents/HomePage.jsx
+++ src/Contents/HomePage.jsx
@@ -6,11 +6,13 @@ import { motion } from 'framer-motion';
 export default function HomePage({ checkScrolledStart }) {
   const [startAnimation, setStartAnimation] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
-
+  const [hideHome, setHideHome] = useState(true);
+  
   useEffect(() => {
     if (checkScrolledStart === 2) {
       setStartAnimation(true);
     } else {
+      setHideHome(false)
       setStartAnimation(false);
     }
   }, [checkScrolledStart]);
@@ -26,21 +28,42 @@ export default function HomePage({ checkScrolledStart }) {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
+  let instantSet = null;
+  let homeHeadersRef = useRef(null);
   const calculateBoxOpacity = () => {
     const viewportHeight = window.innerHeight;
     const scrollPos = window.scrollY;
     const documentHeight = document.documentElement.scrollHeight;
     const startScroll = viewportHeight;
     const endScroll = documentHeight - 2*viewportHeight;
-
+    if(homeHeadersRef && homeHeadersRef.current){
+      homeHeadersRef.current.style.opacity = 1 - scrollPos/viewportHeight;
+    }
     if (scrollPos < startScroll) {
       return 0;
     }
 
     const scrollRange = endScroll - startScroll;
     const scrollProgress = (scrollPos - startScroll) / scrollRange;
+    if(scrollProgress>0.5){
+      if(!hideHome && (instantSet===false||instantSet===null)){
+        instantSet = true;
+        setHideHome(true);
+      }
+    }else{
+      if(hideHome && (instantSet===true||instantSet===null)){
+        
+        instantSet = false;
+        setHideHome(false)
+      }
+    }
     return Math.min(scrollProgress, 1);
+  };
+  const handleDownArrowClick = () => {
+    window.scrollTo({
+      top: window.scrollY + window.innerHeight * 0.9,
+      behavior: 'smooth'
+    });
   };
 
   const boxVariants = {
@@ -56,13 +79,12 @@ export default function HomePage({ checkScrolledStart }) {
       },
     },
   };
-
   return (
     <div id="all-main-section-home-page-parent" className={s.homePageParent}>
-      <div className={s.homeText}>
+      {!hideHome && <div ref={homeHeadersRef} className={s.homeText}>
         <motion.p
           className={s.name}
-          initial={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
           animate={{ opacity: checkScrolledStart === 2 ? 0 : 1 }}
           transition={{ duration: 1, ease: 'easeInOut' }}
         >
@@ -70,7 +92,7 @@ export default function HomePage({ checkScrolledStart }) {
         </motion.p>
         <motion.p
           className={s.jobTitle}
-          initial={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
           animate={{ opacity: checkScrolledStart === 2 ? 0 : 1 }}
           transition={{ duration: 1, ease: 'easeInOut' }}
         >
@@ -78,8 +100,8 @@ export default function HomePage({ checkScrolledStart }) {
         </motion.p>
         <motion.div
           className={s.socialLinks}
-          initial={{ opacity: 1 }}
-          animate={{ opacity: checkScrolledStart === 1 ? 1 : 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: checkScrolledStart === 2 ? 0 : 1 }}
           transition={{ duration: 1, ease: 'easeInOut' }}
         >
           <img
@@ -107,8 +129,8 @@ export default function HomePage({ checkScrolledStart }) {
             alt="Credly"
           />
         </motion.div>
-      </div>
-      <motion.div
+      </div>}
+      {calculateBoxOpacity()!=0 && <motion.div
       className={s.contactMeBoxParent}
         initial="initial"
         animate={startAnimation ? "animate" : "initial"}
@@ -123,8 +145,56 @@ export default function HomePage({ checkScrolledStart }) {
         }}
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ex illo dolorum perspiciatis unde repellat accusantium earum enim mollitia neque debitis, ab, incidunt corrupti nobis nostrum consectetur nam tempore! Earum, impedit. Soluta adipisci blanditiis, nostrum nisi dolores, corporis ipsum amet, fugiat reiciendis excepturi magnam eum eveniet praesentium minima totam aspernatur impedit necessitatibus architecto obcaecati ipsam vel. Doloribus harum tempora temporibus molestias voluptates modi mollitia cumque, pariatur blanditiis fuga facere quas omnis reiciendis excepturi nobis consequatur dignissimos! Iure sit consequuntur molestias ex dolorum sunt, aut dolore, laborum facere quod optio magnam deserunt totam quos placeat repellendus. Deleniti, in. Hic at nobis voluptatem corrupti eaque ea, est, ducimus iusto velit recusandae doloremque ut magni fugiat illo voluptates officia quis natus. Voluptatum cumque repellat officia! Explicabo quas repellendus vel iusto soluta. Expedita aspernatur officia assumenda in eaque dolor, porro quas veniam ratione, voluptatibus odit eius neque nulla et quod atque iure distinctio enim? Ut vero, molestiae laudantium reiciendis deserunt, iure soluta, neque optio necessitatibus nulla expedita vel animi fugiat quasi. Necessitatibus exercitationem voluptatum possimus quam, repellendus ratione nisi officiis, minus optio vero ea quos omnis perspiciatis aspernatur, quasi unde beatae mollitia! Doloremque, esse. Sapiente tenetur explicabo harum repellat corrupti architecto deserunt ex sit ab sequi quod debitis repellendus omnis, consequuntur dolorem saepe a aliquid animi magnam nam, eveniet impedit? Quam quasi quod consectetur obcaecati distinctio placeat consequatur adipisci quae error, doloremque, quisquam ducimus similique! Totam fuga excepturi sapiente recusandae dolores? Neque labore libero architecto error rerum, quibusdam voluptas sapiente aliquam dolore? Doloremque in neque explicabo laborum numquam beatae atque ab earum dignissimos fugiat deleniti consectetur, molestiae cupiditate, quae natus facere vero dolor quidem ad quas. Eum sequi, atque voluptatem sunt quaerat id iste porro tempore quas modi praesentium! Minima optio cupiditate laborum, eaque totam perspiciatis quae voluptate libero doloribus animi reprehenderit, illo nihil. Inventore asperiores culpa tempora nemo veritatis ad velit hic quam quibusdam tempore, cum ipsam voluptatum modi, saepe pariatur fugiat. Ullam, nobis. Corrupti alias eos laudantium ipsam. Praesentium nam deleniti perspiciatis error placeat cum veritatis nostrum. Sint ad voluptatibus accusamus eaque, omnis atque ullam aspernatur ipsa odit maiores animi eveniet deserunt magni voluptate! Unde provident corporis beatae esse voluptas voluptate soluta, nobis consequuntur autem laborum rem repellendus cum alias magni odio sed rerum vitae perferendis tempore similique? Fuga nam odio libero itaque delectus accusantium nesciunt fugiat impedit temporibus exercitationem voluptates ad laborum nemo et enim in velit architecto qui magnam neque laudantium accusamus, sit molestiae. Exercitationem eos quas corrupti culpa ad quasi labore doloribus nemo voluptatem molestias provident incidunt soluta est et, impedit accusamus esse quisquam autem nostrum officia animi? Soluta porro, totam, reprehenderit qui nisi molestiae eius ad, nesciunt consequatur maxime similique culpa itaque neque velit suscipit id fugiat incidunt ab. Cumque, ipsum odit. Nulla perspiciatis expedita distinctio maiores incidunt eum doloribus quod magni sunt illo asperiores, laudantium officiis deleniti temporibus, nostrum ab minus obcaecati dolore dolorum vel placeat sint consectetur excepturi. Natus laboriosam velit repellat minus, ducimus sunt corporis dolor placeat amet, sequi sapiente iure eos? Necessitatibus et dignissimos amet ipsa voluptate quae commodi autem maxime voluptas repudiandae quod magnam labore ab voluptatum, inventore esse consequatur eveniet. Aperiam, deserunt ad minima, ullam ea eos mollitia beatae debitis saepe repellat nam, accusamus fuga? Quam, consequuntur consequatur eum labore numquam praesentium aliquid maiores at dolor velit eaque et neque incidunt nostrum totam officia possimus sunt magni dolorem ea voluptas soluta vel iste. Porro quos in soluta nostrum esse ab pariatur unde excepturi culpa ad, ipsum libero! Exercitationem, pariatur. Doloribus, non modi sed est accusantium repudiandae quos sit. Inventore suscipit nesciunt iste voluptatum ipsum minus sit quae rem! Totam consectetur repellendus et quasi, aut ipsam eius? Neque repellendus, harum, dolore debitis esse pariatur non doloremque ut excepturi, at alias. Aperiam excepturi, quis quo autem quod, ipsam non laborum repellat dicta perferendis rem nesciunt sed harum voluptatibus? Minus doloremque animi laudantium incidunt voluptatum molestias aliquam ut consequatur deserunt a, excepturi ad repellendus minima in veniam fugit praesentium sit quas neque, doloribus veritatis modi et dolorum. Similique, beatae. Praesentium expedita fugiat officiis at obcaecati. Numquam inventore, ad sunt nam eligendi illum eius consectetur nisi veritatis ex, quam, sed repellendus. Exercitationem optio neque odio repellendus ducimus fugit earum autem alias ipsum hic similique fugiat voluptatem maxime laborum, qui velit eos cum obcaecati quidem! Iure pariatur fuga eveniet dolorem voluptatibus quam, nulla, dolorum aspernatur necessitatibus at reiciendis eius reprehenderit. Quod rerum, culpa explicabo consectetur molestias debitis rem possimus unde cumque numquam beatae, obcaecati doloribus error! Voluptatum corrupti, aspernatur placeat ex dolorem iusto doloremque? Voluptatem, et? Quidem, corrupti aut? Beatae, laborum quibusdam eveniet in voluptatum amet sint assumenda atque numquam nemo esse eum reiciendis aliquid consequatur eius nihil quam natus animi saepe alias, culpa corporis non nisi aspernatur. Dolor minima provident ullam nam tenetur. Ad omnis repellat neque dicta facere earum, animi iure pariatur nulla. Minus architecto odio omnis facere, aut consectetur quasi nemo nisi iure magnam tenetur quo laborum mollitia veniam corporis corrupti ab delectus quia nesciunt repudiandae quas asperiores in commodi! Et, quos tempore quam id perferendis delectus, sequi quidem cumque perspiciatis culpa incidunt deserunt nihil voluptatem vel, illo aspernatur similique. Facere distinctio, nesciunt numquam eius eos dolore saepe voluptatem provident odio vitae explicabo aliquam quo voluptatum dignissimos dolor? Porro perspiciatis ullam veritatis temporibus eaque distinctio dolore iusto dolores dicta necessitatibus inventore eligendi nobis doloribus, nihil voluptatem odio at quos alias doloremque soluta nesciunt. Voluptatum deserunt distinctio rerum quidem reprehenderit ducimus, cum optio officia repellat fugit sed facere, ipsum placeat deleniti quaerat asperiores eligendi eveniet at? Tenetur laudantium quaerat quidem amet, provident sit dolore fuga assumenda eaque tempora consequuntur eos dolores cupiditate, doloribus consectetur obcaecati magni nisi qui laborum modi animi voluptas adipisci? Suscipit error quam optio modi dolorum, id voluptatibus, aspernatur perspiciatis expedita quasi ipsam commodi totam autem maiores, temporibus impedit! Quam animi similique, ab, quo asperiores esse reiciendis ipsa optio commodi obcaecati fuga iusto doloremque odio veritatis quia laborum sunt eos aut vitae! Vel, at? Eaque, tempora placeat debitis fugiat obcaecati beatae blanditiis voluptate excepturi ab deleniti, odio cum accusantium at unde molestiae?
-      </motion.div>
-      <DVDthing checkScrolledStart={checkScrolledStart} />
+      </motion.div>}
+      {(!hideHome && checkScrolledStart==1) && <motion.div
+        style={{
+          position: 'fixed',
+          width: '100%',
+          zIndex: 10,
+          bottom: 0,
+          display: 'grid',
+          placeContent: 'center'
+        }}
+        animate={{
+          y: [0, -20, 0],
+        }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          ease: 'easeInOut',
+        }}
+      >
+            <svg
+              onClick={handleDownArrowClick}
+              width="3rem"
+              height="3rem"
+              viewBox="0 0 1024 1024"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              className={s.downArrowSVG}
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                stroke="#797979"
+                strokeWidth="20.48"
+              >
+                <path
+                  d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
+                  fill="#ffffff"
+                />
+              </g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
+                  fill="#ffffff"
+                />
+              </g>
+            </svg>
+      </motion.div>}
+      {!hideHome && <DVDthing checkScrolledStart={checkScrolledStart} />}
     </div>
   );
 }
@@ -220,7 +290,7 @@ const DVDthing = ({checkScrolledStart}) => {
 
   return (
     <motion.div className={s.box} ref={boxRef}
-      initial={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
       animate={{ opacity: checkScrolledStart==1 ? 1 : 0 }}
       transition={{ duration: 1, ease: "easeInOut" }}
     >
