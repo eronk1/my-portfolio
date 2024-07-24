73a0e62bd93979717574950c7dc4ff828a24036b
eronk1 <jeongahkim8174@gmail.com>
2024-07-24 09:28:03 -0700

    fixed tab index for icons

--- src/Contents/EndSec.jsx
+++ src/Contents/EndSec.jsx
@@ -5,16 +5,16 @@ export default function EndSec() {
     <div>
         <div className={s.socialLinks}>
           <a href="https://www.linkedin.com/in/seon-mo-kim/" target="_blank" title="https://www.linkedin.com/in/seon-mo-kim/">
-            <img tabIndex={0} src="/linkedIn.svg" alt="LinkedIn" />
+            <img src="/linkedIn.svg" alt="LinkedIn" />
           </a>
           <a href="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com" target="_blank" title="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com">
-            <img tabIndex={0} src="/gmail.svg" alt="Gmail" />
+            <img src="/gmail.svg" alt="Gmail" />
           </a>
           <a href="https://github.com/eronk1/" target="_blank" title="https://github.com/eronk1/">
-            <img tabIndex={0} src="/github.svg" alt="Github" />
+            <img src="/github.svg" alt="Github" />
           </a>
           <a href="https://www.credly.com/users/seon-kim.37523a11" target="_blank" title="https://www.credly.com/users/seon-kim.37523a11">
-            <img tabIndex={0} src="/credly.svg" alt="Credly" />
+            <img src="/credly.svg" alt="Credly" />
           </a>
         </div>
     </div>
--- src/Contents/HomePage.jsx
+++ src/Contents/HomePage.jsx
@@ -159,16 +159,16 @@ export default function HomePage({ checkScrolledStart, setCheckScrolledStart }) 
           transition={{ duration: 1, ease: 'easeInOut' }}
         >
           <a href="https://www.linkedin.com/in/seon-mo-kim/" target="_blank" title="https://www.linkedin.com/in/seon-mo-kim/">
-            <img tabIndex={0} src="/linkedIn.svg" alt="LinkedIn" />
+            <img src="/linkedIn.svg" alt="LinkedIn" />
           </a>
           <a href="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com" target="_blank" title="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com">
-            <img tabIndex={0} src="/gmail.svg" alt="Gmail" />
+            <img src="/gmail.svg" alt="Gmail" />
           </a>
           <a href="https://github.com/eronk1/" target="_blank" title="https://github.com/eronk1/">
-            <img tabIndex={0} src="/github.svg" alt="Github" />
+            <img src="/github.svg" alt="Github" />
           </a>
           <a href="https://www.credly.com/users/seon-kim.37523a11" target="_blank" title="https://www.credly.com/users/seon-kim.37523a11">
-            <img tabIndex={0} src="/credly.svg" alt="Credly" />
+            <img src="/credly.svg" alt="Credly" />
           </a>
         </motion.div>
       </div>}
@@ -190,16 +190,16 @@ export default function HomePage({ checkScrolledStart, setCheckScrolledStart }) 
         <div className={s.footer}>      
           <div className={s.socialPage2Links}>
             <a href="https://www.linkedin.com/in/seon-mo-kim/" target="_blank" title="https://www.linkedin.com/in/seon-mo-kim/">
-              <img tabIndex={0} src="/linkedIn.svg" alt="LinkedIn" />
+              <img src="/linkedIn.svg" alt="LinkedIn" />
             </a>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com" target="_blank" title="https://mail.google.com/mail/?view=cm&fs=1&to=skimnumber@gmail.com">
-              <img tabIndex={0} src="/gmail.svg" alt="Gmail" />
+              <img src="/gmail.svg" alt="Gmail" />
             </a>
             <a href="https://github.com/eronk1/" target="_blank" title="https://github.com/eronk1/">
-              <img tabIndex={0} src="/github.svg" alt="Github" />
+              <img src="/github.svg" alt="Github" />
             </a>
             <a href="https://www.credly.com/users/seon-kim.37523a11" target="_blank" title="https://www.credly.com/users/seon-kim.37523a11">
-              <img tabIndex={0} src="/credly.svg" alt="Credly" />
+              <img src="/credly.svg" alt="Credly" />
             </a>
           </div>
         </div>
