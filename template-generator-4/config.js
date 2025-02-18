658a9818201ec8cd4e01e27ca1b32399bba4b92b
Eron <jeongahkim8174@gmail.com>
2025-02-18 02:08:02 -0800

    added template generator

---
+++ template-generator-4/config.js
@@ -0,0 +1,15 @@
+/**************************************************************
+ * CONFIG.JS
+ * Contains the default configuration for subjects.
+ **************************************************************/
+var mainData = {
+    "all-subjects": [
+      "Server Errors",
+      "Proxmox Errors",
+      "Proxmox Features",
+      "Fog Server",
+      "Server Features",
+      "Firefox Config"
+    ]
+  };
+  
